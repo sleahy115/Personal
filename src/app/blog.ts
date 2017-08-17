2951dffc083423613e064c5370a1c4c6c2b5423a
sleahy115 <sleahy115@gmail.com>
2017-08-16 19:26:57 -0600

    create blog model

---
+++ src/app/blog.ts
@@ -0,0 +1,11 @@
+export class Blog {
+
+  constructor(
+    public id: number,
+    public title: string,
+    public date: string,
+    public author: string,
+    public entry: string
+  ) {  }
+
+}
