a26038972b34daac0d2df94e0f54cb5f8a86493e
sleahy115 <sleahy115@gmail.com>
2017-08-16 19:48:01 -0600

    add form to admin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing } from './app.routing';
 
+import { AdminComponent } from './admin/admin.component';
 import { AppComponent } from './app.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { BlogComponent } from './blog/blog.component';
+import { BootcampArticleComponent } from './bootcamp-article/bootcamp-article.component';
+import { FormsModule }   from '@angular/forms';
+import { GitArticleComponent } from './git-article/git-article.component';
+import { ProjectsComponent } from './projects/projects.component';
 import { ResumeComponent } from './resume/resume.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { GitArticleComponent } from './git-article/git-article.component';
-import { BootcampArticleComponent } from './bootcamp-article/bootcamp-article.component';
-import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { AdminComponent } from './admin/admin.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     routing
   ],
   providers: [],
